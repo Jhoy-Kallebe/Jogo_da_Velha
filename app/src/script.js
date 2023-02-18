d21e424362acac8b7d8c5c3b39ea3d2b6747fbae
Jhoy-Kallebe <kallebejhoy@gmail.com>
2023-02-18 09:23:31 -0300

    v1.0

--- app/src/script.js
+++ app/src/script.js
@@ -6,6 +6,8 @@ const scoreboard = document.querySelector('.scoreboard');
 
 const body = document.querySelector('.body');
 const form = document.querySelector('.log-game');
+const winGame = document.querySelector('.win-game');
+const playerWinner = document.querySelector('.player-winner');
 
 const namePlayerOne = document.querySelector('#player-one');
 const namePlayerTwo = document.querySelector('#player-two');
@@ -47,12 +49,30 @@ function loadScoreboard () {
     scoreboard.innerText = `${scoreboardOne} X ${scoreboardTwo}`;
 }
 
-function resetGame(){
+function resetGame() {
     arrayChecks.forEach((v, index) => {
         arrayChecks[index] = '';
     });
     loadGame();
     gameWin = '';
+}
+
+function resetGameWin() {
+    resetGame();
+    body.classList.toggle('form');
+    winGame.classList.toggle('active');
+}
+
+function loadGameWin() {
+    loadGame();
+    body.classList.toggle('form');
+    winGame.classList.toggle('active');
+}
+
+function playerWinnerGame() {
+    body.classList.toggle('form');
+    winGame.classList.toggle('active');
+    playerWinner.innerText = `${gameWin} ganhou!`
 }
 
 function checkSpace(index) {
@@ -89,66 +109,64 @@ function playWin(player, sybol) {
 
     if(gameWin == playerOne.value) {
         scoreboardOne++;
-        resetGame();
     }
     if (gameWin == playerTwo.value) {
         scoreboardTwo++;
-        resetGame();
     }
 }
 
 function winColumnOne(player, sybol) {
     if(arrayChecks[0] == sybol && arrayChecks[3] == sybol && arrayChecks[6] == sybol) {
-        alert(player.value+' ganhou!');
         gameWin = player.value;
+        playerWinnerGame();
     }
 }    
 
 function winColumnTwo(player, sybol){
     if(arrayChecks[1] == sybol && arrayChecks[4] == sybol && arrayChecks[7] == sybol) {
-        alert(player.value+' ganhou!');
         gameWin = player.value;
+        playerWinnerGame()
     }
 }
 
 function winColumnThree(player, sybol) {
     if(arrayChecks[2] == sybol && arrayChecks[5] == sybol && arrayChecks[8] == sybol) {
-        alert(player.value+' ganhou!');
         gameWin = player.value;
+        playerWinnerGame()
     }
 }
 
 function winRowOne(player, sybol) {
     if(arrayChecks[0] == sybol && arrayChecks[1] == sybol && arrayChecks[2] == sybol) {
-        alert(player.value+' ganhou!');
         gameWin = player.value;
+        playerWinnerGame()
     }
 }
 
 function winRowTwo(player, sybol) {
     if(arrayChecks[3] == sybol && arrayChecks[4] == sybol && arrayChecks[5] == sybol) {
-        alert(player.value+' ganhou!');
         gameWin = player.value;
+        playerWinnerGame()
     }
 }
 
 function winRowThree(player, sybol) {
     if(arrayChecks[6] == sybol && arrayChecks[7] == sybol && arrayChecks[8] == sybol) {
-        alert(player.value+' ganhou!');
         gameWin = player.value;
+        playerWinnerGame()
     }
 }
 
 function winCrossOne(player, sybol) {
     if(arrayChecks[0] == sybol && arrayChecks[4] == sybol && arrayChecks[8] == sybol) {
-        alert(player.value+' ganhou!');
         gameWin = player.value;
+        playerWinnerGame()
     }
 }
 
 function winCrossTwo(player, sybol) {
     if(arrayChecks[2] == sybol && arrayChecks[4] == sybol && arrayChecks[6] == sybol) {
-        alert(player.value+' ganhou!');
         gameWin = player.value;
+        playerWinnerGame()
     }
 }
